e9cc5c9e5452f67b8651ca3beaf9fad26112a26d
Martin Kennelly <mkennell@redhat.com>
2023-06-27 11:56:35 +0100

    Refactor route manager and fix default 'blackhole' route
    Refactor route manager to reflect it is now its own pkg
    and should be renamed.
    
    Signed-off-by: Martin Kennelly <mkennell@redhat.com>

--- go-controller/pkg/node/node_ip_handler_linux_test.go
+++ go-controller/pkg/node/node_ip_handler_linux_test.go
@@ -12,12 +12,10 @@ import (
 	"github.com/ovn-org/ovn-kubernetes/go-controller/pkg/factory"
 	"github.com/ovn-org/ovn-kubernetes/go-controller/pkg/kube"
 	ovntest "github.com/ovn-org/ovn-kubernetes/go-controller/pkg/testing"
-	"github.com/ovn-org/ovn-kubernetes/go-controller/pkg/types"
 	"github.com/ovn-org/ovn-kubernetes/go-controller/pkg/util"
 
 	"github.com/vishvananda/netlink"
 
-	v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/kubernetes/fake"
