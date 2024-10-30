ovn-org/ovn-kubernetes

EIP CM: add pkt mark to annotations
Signed-off-by: Martin Kennelly <mkennell@redhat.com>

go-controller/pkg/util/egressip_annotation.go
@@ -1,0 +1,69 @@
+package util
+
+import (
+	"fmt"
+	"math"
+	"strconv"
+)
+
+const (
+	EgressIPMarkAnnotation = "k8s.ovn.org/egressip-mark"
+	EgressIPMarkBase       = 50000
+	EgressIPMarkMax        = math.MaxUint16
+)
+
+type EgressIPMark struct {
+	s string
+	i int
+}
+
+func (em EgressIPMark) ToString() string {
+	return em.s
+}
+
+func (em EgressIPMark) ToInt() int {
+	return em.i
+}
+
+func (em EgressIPMark) IsValid() bool {
+	return IsEgressIPMarkValid(em.i)
+}
+
+func (em EgressIPMark) IsAvailable() bool {
+	return em.s != ""
+}
+
+func ParseEgressIPMark(annotations map[string]string) (EgressIPMark, error) {
+	eipMark := EgressIPMark{}
+	if annotations == nil {
+		return eipMark, fmt.Errorf("failed to parse EgressIP mark from annotation because annotation is nil")
+	}
+	markStr, ok := annotations[EgressIPMarkAnnotation]
+	if !ok {
+		return eipMark, nil
+	}
+	eipMark.s = markStr
+	mark, err := strconv.Atoi(markStr)
+	if err != nil {
+		return eipMark, fmt.Errorf("failed to parse EgressIP mark annotation string %q to an integer", markStr)
+	}
+	eipMark.i = mark
+	return eipMark, nil
+}
+
+func IsEgressIPMarkSet(annotations map[string]string) bool {
+	if annotations == nil {
+		return false
+	}
+	_, ok := annotations[EgressIPMarkAnnotation]
+	return ok
+}
+
+func IsEgressIPMarkValid(mark int) bool {
+	return mark >= EgressIPMarkBase && mark <= EgressIPMarkMax
+}
+
+// EgressIPMarkAnnotationChanged returns true if the EgressIP mark annotation changed
+func EgressIPMarkAnnotationChanged(annotationA, AnnotationB map[string]string) bool {
+	return annotationA[EgressIPMarkAnnotation] != AnnotationB[EgressIPMarkAnnotation]
+}

martinkennelly/ovn-kubernetes-1

go-controller/pkg/util/egressip_annotation.go
@@ -1,0 +1,69 @@
+package util
+
+import (
+	"fmt"
+	"math"
+	"strconv"
+)
+
+const (
+	EgressIPMarkAnnotation = "k8s.ovn.org/egressip-mark"
+	EgressIPMarkBase       = 50000
+	EgressIPMarkMax        = math.MaxUint16
+)
+
+type EgressIPMark struct {
+	strValue string
+	intValue int
+}
+
+func (em EgressIPMark) String() string {
+	return em.strValue
+}
+
+func (em EgressIPMark) ToInt() int {
+	return em.intValue
+}
+
+func (em EgressIPMark) IsValid() bool {
+	return IsEgressIPMarkValid(em.intValue)
+}
+
+func (em EgressIPMark) IsAvailable() bool {
+	return em.strValue != ""
+}
+
+func ParseEgressIPMark(annotations map[string]string) (EgressIPMark, error) {
+	eipMark := EgressIPMark{}
+	if annotations == nil {
+		return eipMark, fmt.Errorf("failed to parse EgressIP mark from annotation because annotation is nil")
+	}
+	markStr, ok := annotations[EgressIPMarkAnnotation]
+	if !ok {
+		return eipMark, nil
+	}
+	eipMark.strValue = markStr
+	mark, err := strconv.Atoi(markStr)
+	if err != nil {
+		return eipMark, fmt.Errorf("failed to parse EgressIP mark annotation string %q to an integer", markStr)
+	}
+	eipMark.intValue = mark
+	return eipMark, nil
+}
+
+func IsEgressIPMarkSet(annotations map[string]string) bool {
+	if annotations == nil {
+		return false
+	}
+	_, ok := annotations[EgressIPMarkAnnotation]
+	return ok
+}
+
+func IsEgressIPMarkValid(mark int) bool {
+	return mark >= EgressIPMarkBase && mark <= EgressIPMarkMax
+}
+
+// EgressIPMarkAnnotationChanged returns true if the EgressIP mark annotation changed
+func EgressIPMarkAnnotationChanged(annotationA, annotationB map[string]string) bool {
+	return annotationA[EgressIPMarkAnnotation] != annotationB[EgressIPMarkAnnotation]
+}